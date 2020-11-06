72fc218153254e0f010404f65961d63755fa4e44
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2020-11-06 22:31:49 +0200

    added functionality to run drum kit on click to a specific btn as well

--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
 "use strict";
 const runScript = () => {
 
-   const runSound = (event) => {
-      const physicalKey = event.code; // get a particular pressed key (physical key on the keyboard)
-    
-      const sound = document.querySelector(`audio[data-key="${physicalKey}"]`) 
-      const key = document.querySelector(`button[data-key="${physicalKey}"]`);
+   const runSound = (key, type) => {
+      const characterCode = getCharacterCode(key, type);
 
+      const sound = document.querySelector(`audio[data-key="${characterCode}"]`);
+      const keyBtn = document.querySelector(`button[data-key="${characterCode}"]`);
 
       if (sound) { // if specific sound responds to a correspond key (in data-key attribute) then we play sound
 
          sound.currentTime = 0; // rewind a sound (to start from the beginning immediately)
          sound.play();
 
-         key.classList.add("active");
+         keyBtn.classList.add("active");
+      }
+   };
+
+   //get the keycode from a keypress or click events
+   function getCharacterCode(key, type) {
+      if (type === "click") {
+         return soundOnClick(key);
+      } else if (type === "press") {
+         return soundOnPress(key);
       }
    }
 
+   //get a particular keycode
+   function soundOnPress(key) {
+      const keyCode = key.keyCode;
+      return keyCode;
+   }
 
-   function removeTransition(e) {
-      if (e.propertyName !== "transform") return;
+   //get a particular clicked btn with a certain data-key value
+   function soundOnClick(key) {
+      const keyCode = key.path[0].getAttribute("data-key");
+      return keyCode;
+   }
+
+
+   function removeTransition(event) {
+
+      if (event.propertyName !== "transform") return;
 
       this.classList.remove("active"); // remove an active class from a certain pressed key
    };
 
-   const removeActiveClass = () => {
+   function removeActiveClass() {
       const keys = document.querySelectorAll(".drum-kit__key");
 
       keys.forEach(key => {
@@ -32,10 +53,19 @@ const runScript = () => {
       });
    };
 
-   document.addEventListener("keydown", runSound);
+   // Add an event listeners in order to listen to a keypress or click
+   window.addEventListener("keydown", (key) => {
+      const type = "press";
+      runSound(key, type);
+   });
 
-   //call functions
-   removeActiveClass(); 
+   window.addEventListener("click", (key) => {
+      const type = "click";
+      runSound(key, type);
+   });
+
+   // call functions
+   removeActiveClass();
 }
 
 if (document.readyState === "loading") {
